09cc0f3e56881e8f1155b484bfa10b0f4590ebeb
Chris911 <Christophe.naud.dulude@gmail.com>
2013-04-23 22:38:04 -0400

    Removed sending loop. Use BCC field instead.

--- bin/github-drinkup-notifier.rb
+++ bin/github-drinkup-notifier.rb
@@ -2,10 +2,15 @@ require 'gmail'
 require 'feedzirra'
 
 # fetching a single feed
-LAST_MODIFIED_TEST = Time.new(2013, 04, 21)
-EMAILS_TEST = {"Firenze" => ["Christophe.naud.dulude@gmail.com", "christophe911@hotmail.com"]}
+LAST_MODIFIED_TEST = Time.new(2013, 02, 21)
+EMAILS_TEST = {"Firenze" => ["Christophe.naud.dulude@gmail.com", "christophe911@hotmail.com"],
+"London" => ["christophe911@hotmail.com"]}
 
-gmail = Gmail.connect('github.drinkup.notifier','hello_dave')
+gmail = Gmail.connect('github.drinkup.notifier','')
+unless gmail.logged_in?
+  puts "Wrong Email/Password Combination"
+  exit(1)
+end
 feed = Feedzirra::Feed.fetch_and_parse("https://github.com/blog/drinkup.atom")
 
 if feed.last_modified > LAST_MODIFIED_TEST
@@ -16,13 +21,12 @@ if feed.last_modified > LAST_MODIFIED_TEST
     next if city.nil?
     if EMAILS_TEST.has_key? city
       emails = EMAILS_TEST[city]
-      emails.each do |email|
-        gmail.deliver do
-          to   email
-          from 'GitHub Drinkup Notifier'
-          subject "GitHub Drinkup Notifier : #{city}"
-          body "There seems to be a GitHub drinkup if your city!\nMore at #{entry.url}"
-        end
+      gmail.deliver do
+        to      'github.drinkup.notifier@gmail.com'
+        bcc      emails.join(", ")
+        from    'GitHub Drinkup Notifier'
+        subject "GitHub Drinkup Notifier : #{city}"
+        body    "There seems to be a GitHub drinkup if your city!\nMore at #{entry.url}"
       end
     end
   end
