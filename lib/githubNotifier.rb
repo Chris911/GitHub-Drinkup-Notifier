357ec3da4526d34c45e04ba6708a7d12071827df
Christophe Naud-Dulude <christophe.naud.dulude@gmail.com>
2014-07-20 01:40:06 -0700

    Remove debug line

--- lib/githubNotifier.rb
+++ lib/githubNotifier.rb
@@ -21,14 +21,14 @@ class GithubNotifier
       feed.entries.take_while{ |entry| entry.last_modified > @last_checked }.each do |entry|
         # String transliteration (Removed accents)
         title = entry.title.removeaccents.downcase
-        puts title
+        
         # Match city in blog entry
         city = title[/([a-zA-Z\s]*)\s[dD]rinkup/,1]          if title =~ /.*\s[dD]rinkup/
         city = title[/[dD]rinkup\s[iI]n\s([a-zA-Z\s]*).*/,1] if title =~ /[dD]rinkup\s[iI]n\s.*/
         city = title[/([a-zA-Z\s]*)\s[mM]eetup/,1]           if title =~ /.*\s[mM]eetup/
         city = title[/[mM]eetup\s[iI]n\s([a-zA-Z\s]*).*/,1]  if title =~ /[mM]eetup\s[iI]n\s.*/
         next if city.nil?
-        alerts = @config['alerts'].find_all{|alert| alert['cities'].map(&:downcase).include?(city.downcase.strip!  ) }
+        alerts = @config['alerts'].find_all{ |alert| alert['cities'].map(&:downcase).include?(city.downcase.strip!) }
 
         unless alerts.empty?
           emails = alerts.collect{ |alert| alert['email'] }
